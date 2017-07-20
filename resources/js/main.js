bfbe4e5c4bc4fe79dcaebcacb989e6cc1d72d6ad
Katie <katherinelane11@gmail.com>
2017-07-20 15:57:54 -0400

    adds beginning of hamburger menu with icons

---
+++ resources/js/main.js
@@ -0,0 +1,13 @@
+// when hamburger menu is clicked:
+// 1. hamburger icon goes to display: none;
+// 2. cross icon goes to display: unset;
+// 3. menu goes from display: none to display: unset;
+
+
+
+var $MENU_CONTAINER = $('[data-text-role="menu"]')
+var $EXIT_CONTAINER = $('[data-image-role="exit-container"]')
+var $HAMBURGER = $('[data-image-role="hamburger"]')
+
+
+
